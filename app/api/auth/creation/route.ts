834f7e640685b47df5712392144bb6c2b8096e75
Viktoria <bukovszki.viktoria@gmail.com>
2024-05-30 20:40:44 +0200

    Changed urls for deployment

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -32,5 +32,5 @@ export async function GET() {
     });
   }
 
-  return NextResponse.redirect("http://localhost:3000");
+  return NextResponse.redirect("https://airbnb-clone-next-alpha.vercel.app");
 }
